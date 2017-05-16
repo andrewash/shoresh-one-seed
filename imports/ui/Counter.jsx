acfe922af47389d0203095c7ca037653c70152ea
Andrew Ash <andrew@emotiveapps.com>
2017-05-15 23:39:41 -0700

    Finished second screen

--- imports/ui/Counter.jsx
+++ imports/ui/Counter.jsx
@@ -5,7 +5,9 @@ export default class Counter extends Component {
   render() {
     return (
       <div className="counter">
-        <h1>{this.props.count} seedlings by JEDX</h1>
+        <h1>{this.props.count} Seedlings<br/>by JEDX</h1>
+        <img className="power-of-one-seed" src="power-of-one-seed.png" />
+        <img className="logo" src="logo-rectangle.png" />
       </div>
     );
   }
